8c18ba1668ac7f0d6bdc79664776555ad303875c
VictoriaCarvalho <vix219@icloud.com>
2025-03-14 12:06:59 -0500

    add lab

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,28 @@
 #!/usr/bin/env python3
 
+
+
+
 class Book:
-    pass
+    def __init__(self, title, page_count):
+        self.title = title
+        self._page_count = None
+        self.set_page_count(page_count)
     
-        
+    def set_page_count(self, page_count):
+        if not isinstance(page_count, int):
+            print("page_count must be an integer")
+        else:
+            self._page_count = page_count
+
+    @property
+    def page_count(self):
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, value):
+        self.set_page_count(value)
+    
+    def turn_page(self, turn_page=69):
+        if isinstance(turn_page, int):
+            print("Flipping the page...wow, you read fast!")
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand= "Adidas", size=9, condition="New"):
+        self.brand = brand
+        self.size = size
+        self.condition = condition
+
+    def set_size(self, size):
+        if not isinstance(size, int):
+            print("size must be an integer")
+        else:
+            self._size = size
+    @property
+    def size(self):
+        return self._size
+    @size.setter
+    def size(self, value):
+        self.set_size(value)
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+
